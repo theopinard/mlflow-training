f4a3be192547906b1df804d7641224399c76d690
theopinard <theopinard@hotmail.fr>
2023-04-27 21:53:24 +0000

    apply black

--- src/steps/train.py
+++ src/steps/train.py
@@ -1,8 +1,8 @@
 from typing import Dict, Any
 from sklearn.linear_model import LinearRegression
 
-def estimator_fn(estimator_params: Dict[str, Any] = None):
 
+def estimator_fn(estimator_params: Dict[str, Any] = None):
     if estimator_params is None:
         estimator_params = {}
     return LinearRegression(**estimator_params)
--- tests/steps/test_ingest.py
+++ tests/steps/test_ingest.py
@@ -1,6 +1,7 @@
 from pandas import DataFrame
 from src.steps.ingest import load_dataframe
 
+
 def test_load_dataframe():
     """It load a pandas dataframe successfully"""
     df = load_dataframe("", "")
