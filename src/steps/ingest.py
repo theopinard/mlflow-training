4214249a41aaf6e4fe48044fe5270541b1095e0a
theodore.meynard <theodore.meynard@getyourguide.com>
2023-05-31 22:49:07 +0200

    remove code to implement

--- src/steps/ingest.py
+++ src/steps/ingest.py
@@ -6,5 +6,5 @@ def load_file_as_dataframe(file_path: str, file_format: str) -> DataFrame:
     """Load a csv file as a dataframe and add a column to indicate if the wine is red or white"""
     df = pd.read_csv(file_path, sep=";")
     # ToDo1: Add a column to indicate if the wine is red or white
-    df["is_red"] = 1 if "red" in str(file_path) else 0
+    ...
     return df
--- src/steps/train.py
+++ src/steps/train.py
@@ -5,6 +5,5 @@ from sklearn.linear_model import LinearRegression
 
 def estimator_fn(estimator_params: Dict[str, Any] = None):
     # ToDo4: Create a LinearRegression estimator with the estimator_params
-    if estimator_params is None:
-        estimator_params = {}
-    return LinearRegression(**estimator_params)
+    ...
+    return None
